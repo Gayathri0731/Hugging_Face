3fa17fd2c7d590608b975a31e57b533b8cc9e93e
Gayathri M <gayathrim0731@gmail.com>
2024-10-22 09:45:11 +0530

    commited

---
+++ pipe3.py
@@ -0,0 +1,20 @@
+from transformers import pipeline #type:ignore
+
+classifier = pipeline('zero-shot-classification')
+
+res = classifier(
+    'I have completed this course',
+    candidate_labels=['education','politics','business']
+)
+
+print(res)
+
+# from transformers import pipeline
+
+# classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+# candidate_labels = ['education', 'politics', 'business']
+
+# result = classifier("there is a great loss in our company", candidate_labels)
+
+# print(result)
